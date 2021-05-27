95cc21626cefbb915527d12b21ba17f657a7683e
Ansgar Grunseid <grunseid@gmail.com>
2021-05-26 23:04:46 -0700

    groupin' away

---
+++ index.js
@@ -0,0 +1,91 @@
+const cl = console.log
+
+const $btn = document.getElementById('submit')
+const $input = document.getElementById('input')
+const $output = document.getElementById('output')
+
+function enquote (s) {
+    return '"' + s + '"'
+}
+
+function unquote (s) {
+    if (s.length >= 2 && (
+        (s[0] === '"' && s[s.length - 1] === '"') ||
+        (s[0] === "'" && s[s.length - 1] === "'")))
+        return s.slice(1, -1)
+    return s
+}
+
+function groupContainsValue(group, person, column) {
+    for (const member of group) {
+        //cl('person[column]', person[column], 'member[column]', member[column])
+        if (person[column] === member[column]) {
+            return true
+        }
+    }
+    return false
+}
+
+function sortIntoGroups () {
+    const groupSize = parseInt(document.getElementById('group-size').value)
+
+    const lines = $input.value.split('\n')
+    const numGroups = Math.ceil(lines.length / groupSize)
+
+    const groups = []
+    for (let i = 0; i < groupSize; i++) {
+        groups[i] = []
+    }
+    
+    function addPersonToFirstOpenGroup (person) {
+        for (const group of groups) {
+            if (group.length < groupSize) {
+                group.push(person)
+                break
+            }
+        }
+    }
+
+    function addPersonToFirstUniqueGroup (person, column) {
+        for (const group of groups) {
+            if (!groupContainsValue(group, person, column)
+                && group.length < groupSize) {
+                group.push(person)
+                return true
+            }
+        }
+
+        return false
+    }
+
+    for (const line of lines) {
+        if (!line.replace(/\s/g, '').length) { // whitespace
+            continue
+        }
+
+        // lastName, firstName, school, interest, startDate
+        const person = line.split(',').map(unquote)
+
+        const addedBySchool = addPersonToFirstUniqueGroup(person, 2)
+        cl('addedBySchool', addedBySchool, person)
+        if (!addedBySchool) {
+            const addedByInterest = addPersonToFirstUniqueGroup(person, 3)
+            if (!addedByInterest) {
+                addPersonToFirstOpenGroup(person)
+            }
+        }
+    }
+
+    let output = []
+    for (const group of groups) {
+        for (const member of group) {
+            output.push(member.map(enquote).join(','))
+        }
+        output.push('\n')
+    }
+    $output.value = output.join('\n')
+}
+
+$btn.addEventListener('click', sortIntoGroups)
+
+
